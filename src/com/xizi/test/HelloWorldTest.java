5a5b19a1bed044329c265aa86a1c8a8980c9f590
xizikan <firekan@163.com>
2016-11-01 15:14:26 +0800

    修改编码

--- src/com/xizi/test/HelloWorldTest.java
+++ src/com/xizi/test/HelloWorldTest.java
@@ -1,11 +1,11 @@
 package com.xizi.test;
 
 public class HelloWorldTest {
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println("²âÊÔÒ»ÏÂ");
-		
+
+		System.out.println("æµ‹è¯•");
+
 		System.out.println("hello,world");
 	}
 
